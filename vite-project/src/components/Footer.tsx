dd5fa3cdb1498e36799bd24d238cbc63efb2a7d3
Kartikeya <157278252+KartikeyaNainkhwal@users.noreply.github.com>
2025-04-09 17:27:43 +0530

    Update Footer.tsx

--- vite-project/src/components/Footer.tsx
+++ vite-project/src/components/Footer.tsx
@@ -3,7 +3,7 @@ function Footer() {
       <footer className="footer">
         <div className="container">
           <div className="footer-inner">
-            <div className="footer-logo">Smartpay</div>
+            <div className="footer-logo">Stablepay</div>
             <div className="footer-text">
               <p>Enabling seamless onchain USDC payments between wallet issuers and merchant acquirers</p>
               <p>© 2023 Stablepay. All rights reserved.</p>
